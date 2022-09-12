b50ec85eaf4907dd842ee780c40ddf4d48ad143b
Vítor Norberto <vithor.norberto@gmail.com>
2022-09-12 11:30:20 -0300

    Adiciona modelmapper e dto de funcionario

--- src/main/java/com/vitu/mysqljsonapi/controller/FuncionarioController.java
+++ src/main/java/com/vitu/mysqljsonapi/controller/FuncionarioController.java
@@ -1,7 +1,9 @@
 package com.vitu.mysqljsonapi.controller;
 
 import com.vitu.mysqljsonapi.domain.Funcionario;
+import com.vitu.mysqljsonapi.domain.FuncionarioDto;
 import com.vitu.mysqljsonapi.repository.FuncionarioRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,14 +16,17 @@ public class FuncionarioController {
     @Autowired
     FuncionarioRepository funcionarioRepository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @GetMapping
     public List<Funcionario> getAll() {
         return funcionarioRepository.findAll();
     }
 
     @PostMapping
-    public Funcionario create(@RequestBody Funcionario funcionario) {
-        return funcionarioRepository.save(funcionario);
+    public Funcionario create(@RequestBody FuncionarioDto funcionarioDto) {
+        return funcionarioRepository.save(modelMapper.map(funcionarioDto, Funcionario.class));
     }
 
 }
---
+++ src/main/java/com/vitu/mysqljsonapi/domain/FuncionarioDto.java
@@ -0,0 +1,13 @@
+package com.vitu.mysqljsonapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioDto {
+    private Endereco endereco;
+}
